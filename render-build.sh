8a007eb11fe1b85b055c989e255d7e9e21434669
Tushar Kumar <tushargola1234@gmail.com>
2025-02-16 20:19:58 +0530

    Added render build script for PyAudio

--- render-build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Update package list
-apt-get update
-
-# Install PortAudio (required for PyAudio)
-apt-get install -y portaudio19-dev
-
-# Install Python dependencies
-pip install -r requirements.txt
